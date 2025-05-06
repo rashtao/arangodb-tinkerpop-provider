a24e398d23fc9b7098fdc0f10d46ac119318a179
Michele Rastelli <michele@arangodb.com>
2025-05-06 22:43:49 +0200

    review supported types

--- src/main/java/com/arangodb/tinkerpop/gremlin/structure/ArangoDBGraphFeatures.java
+++ src/main/java/com/arangodb/tinkerpop/gremlin/structure/ArangoDBGraphFeatures.java
@@ -1,14 +1,18 @@
 package com.arangodb.tinkerpop.gremlin.structure;
 
-import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.*;
 import org.apache.tinkerpop.gremlin.structure.util.StringFactory;
 
 public class ArangoDBGraphFeatures implements Graph.Features {
 
     public static class ArangoDBGraphGraphFeatures implements GraphFeatures {
-
         @Override
         public boolean supportsComputer() {
+            return false;
+        }
+
+        @Override
+        public boolean supportsTransactions() {
             return false;
         }
 
@@ -18,13 +22,56 @@ public class ArangoDBGraphFeatures implements Graph.Features {
         }
 
         @Override
-        public boolean supportsTransactions() {
+        public boolean supportsOrderabilitySemantics() {
+            return false;
+        }
+
+        public VariableFeatures variables() {
+            return new ArangoGraphVariableFeatures();
+        }
+    }
+
+    public static class ArangoGraphVariableFeatures extends ArangoDBGraphDataTypeFeatures implements VariableFeatures {
+    }
+
+    public static class ArangoDBGraphDataTypeFeatures implements DataTypeFeatures {
+        @Override
+        public boolean supportsByteValues() {
+            return false;
+        }
+
+        @Override
+        public boolean supportsByteArrayValues() {
             return false;
         }
     }
 
     public static class ArangoDBGraphElementFeatures implements ElementFeatures {
+        @Override
+        public boolean supportsAnyIds() {
+            return false;
+        }
 
+        @Override
+        public boolean supportsCustomIds() {
+            return false;
+        }
+
+        @Override
+        public boolean supportsNumericIds() {
+            return false;
+        }
+
+        @Override
+        public boolean supportsUuidIds() {
+            return false;
+        }
+    }
+
+    public static class ArangoDBGraphPropertyFeatures extends ArangoDBGraphDataTypeFeatures implements PropertyFeatures {
+    }
+
+    public static class ArangoDBGraphVertexPropertyFeatures extends ArangoDBGraphPropertyFeatures implements VertexPropertyFeatures {
         @Override
         public boolean supportsAnyIds() {
             return false;
@@ -47,36 +94,18 @@ public class ArangoDBGraphFeatures implements Graph.Features {
     }
 
     public static class ArangoDBGraphVertexFeatures extends ArangoDBGraphElementFeatures implements VertexFeatures {
-
         @Override
         public VertexPropertyFeatures properties() {
             return new ArangoDBGraphVertexPropertyFeatures();
         }
     }
 
-    public static class ArangoDBGraphEdgeFeatures extends ArangoDBGraphElementFeatures implements EdgeFeatures {
+    public static class ArangoDBGraphEdgePropertyFeatures extends ArangoDBGraphPropertyFeatures implements EdgePropertyFeatures {
     }
 
-    public static class ArangoDBGraphVertexPropertyFeatures implements VertexPropertyFeatures {
-
-        @Override
-        public boolean supportsAnyIds() {
-            return false;
-        }
-
-        @Override
-        public boolean supportsCustomIds() {
-            return false;
-        }
-
-        @Override
-        public boolean supportsNumericIds() {
-            return false;
-        }
-
-        @Override
-        public boolean supportsUuidIds() {
-            return false;
+    public static class ArangoDBGraphEdgeFeatures extends ArangoDBGraphElementFeatures implements EdgeFeatures {
+        public EdgePropertyFeatures properties() {
+            return new ArangoDBGraphEdgePropertyFeatures();
         }
     }
 
